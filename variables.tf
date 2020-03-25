0c683e4079f6187be4d8925382af18ffc7b2460f
Federico Pires <federico.pires@upsight.com>
2020-03-24 21:40:04 -0300

    Make internet gateway creation optional

--- variables.tf
+++ variables.tf
@@ -74,3 +74,8 @@ variable "enable_default_security_group_with_custom_rules" {
   default     = true
 }
 
+variable "enable_internet_gateway" {
+  type        = bool
+  description = "A boolean flag to enable/disable the creation of an internet gateway"
+  default     = true
+}
